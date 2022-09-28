22e06c29de80fde49341571c6f8eb3ec604c58c9
Hussein Ali - حسين علي <111470519+thisishusseinali@users.noreply.github.com>
2022-09-28 11:13:14 +0200

    Add files via upload

---
+++ extractor.py
@@ -0,0 +1,41 @@
+import cv2 as cv
+import numpy as np
+from keras import models
+
+
+class DigitExtractor:
+    def __init__(self):
+        self.cells = []
+        self.string = ''
+
+    def splitGrid(self, frame):
+        # perform additional preprocessing if required
+        rows = np.array_split(ary=frame, indices_or_sections=9, axis=0)
+        for idx, row in enumerate(rows):
+            columns = np.array_split(ary=row, indices_or_sections=9, axis=1)
+            for jdx, column in enumerate(columns):
+                resized_img = cv.resize(column[3:-3, 3:-3],  # the 3:-3 slice removes the borders from each image
+                                        dsize=(28, 28),
+                                        interpolation=cv.INTER_CUBIC)
+                cv.imwrite(
+                    filename=f'vudoku/assets/images/out/cells/cell{idx}{jdx}.jpg', img=resized_img)
+                self.cells.append(resized_img)
+
+    def genString(self):
+        # check if model exists if not, prompt user to run classifier
+        model = models.load_model(filepath='vudoku/assets/models/mnist_trained_model_v1.h5')
+
+        for cell in self.cells:
+            # Mark empty cells are marked as zero the converted into a string
+            rs_cell = cell.reshape(1, 28, 28, 1)
+            predictions = model.predict(rs_cell)
+            digit = predictions[0].tolist().index(max(predictions[0].tolist()))
+            self.string += str(digit)
+
+    def miner(self, image):
+        self.splitGrid(frame=image)
+        self.genString()
+        return self.string
+
+
+dex = DigitExtractor()
---
+++ solver.py
@@ -0,0 +1,85 @@
+import numpy as np
+
+
+class SudokuSolver:
+    def __init__(self):
+        self.board = None
+
+    def solveRecursively(self, board):
+
+        cell = self.isEmptyCell(self.board)
+
+        if not cell:
+            return True
+        else:
+            row, col = cell
+
+        for sol in range(1, 10):
+            if self.isValid(self.board, sol, (row, col)):
+                self.board[row, col] = sol
+                if self.solveRecursively(self.board):
+                    return True
+            self.board[row, col] = 0
+
+        return False
+
+    def isValid(self, board, num, pos):
+        # check row
+        if num in self.board[pos[0]]:
+            return False
+        # check column
+        if num in self.board[..., pos[1]]:
+            return False
+
+        swapped = np.swapaxes(np.reshape(
+            self.board, (3, 3, 3, 3)), 1, 2).reshape(9, 9)
+
+        x = (pos[0] // 3) * 3 + 3
+        y = (pos[1] // 3) * 3 + 3
+        z = 0
+
+        print(x, y)
+
+        if x == 3:
+            if y == 3:
+                z = 0
+            elif y == 6:
+                z = 1
+            else:
+                z = 2
+        elif x == 6:
+            if y == 3:
+                z = 3
+            elif y == 6:
+                z = 4
+            else:
+                z = 5
+        elif y == 3:
+            z = 6
+        elif y == 6:
+            z = 7
+        else:
+            z = 8
+
+        print(swapped[z])
+        if num in swapped[z]:
+            return False
+
+        return True
+
+    def isEmptyCell(self, board):
+        for idx in range(len(self.board)):
+            for jdx in range(len(self.board[0])):
+                if self.board[idx, jdx] == 0:
+                    return (idx, jdx)  # row, col
+        return None
+
+    def gateway(self, string='780400120600075009000601078007040260001050930904060005070300012120007400049206007'):
+        self.board = np.array(list(string), dtype=np.int32).reshape(9, 9)
+        # print(self.board, end="**\n")
+        self.solveRecursively(self.board)
+        return self.board
+
+
+slr = SudokuSolver()
+print(slr.gateway())
